b50769d2f94f304046dbaa5de6572265e4d5de43
Rajeev Dewangan <63413883+rajeevDewangan@users.noreply.github.com>
2025-09-13 15:40:43 +0530

    edge runtime

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+export const runtime = 'edge';
+
 export async function POST(request: NextRequest) {
   try {
     const { to_email, from_name, from_email, message, subject } = await request.json();
